7c9953387a2f827573f4db8684e45d93fad2a03a
egi-megi <egi.megi@gmail.com>
2018-07-16 15:12:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/bookstoreapp/EditorActivity.java
@@ -0,0 +1,8 @@
+package com.example.android.bookstoreapp;
+
+/**
+ * Created by egi-megi on 16.07.18.
+ */
+
+public class EditorActivity {
+}
---
+++ app/src/main/java/com/example/android/bookstoreapp/data/BookContract.java
@@ -0,0 +1,72 @@
+package com.example.android.bookstoreapp.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by egi-megi on 06.07.18.
+ */
+
+public final class BookContract {
+
+    private BookContract() {}
+
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.bookstoreapp";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_BOOKS = "books";
+
+    /**
+     * Inner class that defines constant values for the books database table.
+     * Each entry in the table represents a single book.
+     */
+
+    public static abstract class PetsList implements BaseColumns {
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of pets.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single pet.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_BOOKS;
+
+
+        /** The content URI to access the pet data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_BOOKS);
+
+
+        public static abstract class BookDatabaseTitles implements BaseColumns {
+        public static final String TABLE_NAME = "books";
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_BOOK_TITLE = "TitleBook";
+        public static final String COLUMN_BOOK_AUTHOR = "AuthorBook";
+        public static final String COLUMN_BOOK_PRICE = "Price";
+        public static final String COLUMN_BOOK_QUANTITY = "Quantity";
+        public static final String COLUMN_BOOK_SUPPLIER_NAME = "SupplierName";
+        public static final String COLUMN_BOOK_SUPPLIER_PHONE = "SupplierPhoneNumber";
+
+    }
+}
